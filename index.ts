05bf7cffaebd82507e55c4115c37e657fcdf30d4
Johan Ossanes <johannossanes@gmail.com>
2025-05-17 01:08:30 -0300

    ajustado index

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 import express from 'express'
+import clienteRouter from './routes/cliente'
+import medicamentosRouter from './routes/medicamentos'
+import vendaRouter from './routes/venda'
+import itemVendaRouter from './routes/itemVenda'
+
 const app = express()
 const port = 3000 
 
@@ -6,6 +11,15 @@ app.get('/', (req, res) => {
     res.send('API Farmacia')
 })
 
+app.use(express.json())
+
+app.use('/cliente', clienteRouter)
+
+app.use('/medicamentos', medicamentosRouter )
+
+app.use('/venda', vendaRouter)
+
+app.use('/item-venda', itemVendaRouter)
 app.listen(port, () => {
     console.log(`App rordando na port ${port}`)
 })
